fb2f0b2cd796aea55d65849246049277e3392a0b
MHIZ EUNICE <MHIZ EUNICE>
2019-11-15 19:57:37 -0800

    modify fetch.php file

---
+++ fetch.php
@@ -0,0 +1,20 @@
+<?php
+class user extends dbh {
+    
+    protected function getallusers(){
+        $sql = "select * from student";
+        $result = $this->connect()->query($sql);
+        $numRows = $result->num_rows;
+        if ($numRows > 0){
+            while($row = $result->fetch_assoc()) {
+                $data[] = $row;                
+            }
+            return $data;
+        }
+    }
+    }
+?>     
+                
+              
+
+
